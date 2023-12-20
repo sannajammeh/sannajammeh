6f26318decfab0fda15f6870ab065f3834ef5b6e
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2023-12-21 00:48:54 +0100

    chore: add sitemap

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,29 +1,24 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-
 import solidJs from "@astrojs/solid-js";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sannajammeh.com",
-  integrations: [
-    UnoCSS({
-      injectReset: true,
-      content: {
-        pipeline: {
-          include: [
-            /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
-            "src/**/*.{js,ts}",
-          ],
-        },
-      },
-    }),
-    solidJs(),
-  ],
+  integrations: [UnoCSS({
+    injectReset: true,
+    content: {
+      pipeline: {
+        include: [/\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/, "src/**/*.{js,ts}"]
+      }
+    }
+  }), solidJs(), sitemap()],
   vite: {
     css: {
-      transformer: "lightningcss",
-    },
+      transformer: "lightningcss"
+    }
   },
-  experimental: {},
+  experimental: {}
 });
